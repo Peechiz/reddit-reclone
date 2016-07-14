da49d1d40b67c34d358123be2330e2c83c0c7da6
Chris Impicciche <peechiz@bakudan.local>
2016-07-14 13:55:20 -0500

    adds AddPost and AddComment features to controller

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 var app = angular.module('reddit',[]);
 
-app.controller('main',main)
+app.controller('newPostController',post)
 
-function main(){
-  var m = this;
+function post(){
+  var np = this;
+
+  np.posts = [
+    {
+      title: 'Using Angular',
+      author: 'Bill Murray',
+      img: 'http://fillmurray.com/200/200',
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      score: 5,
+      comments: []
+    }
+  ];
+
+  var clearForm = function(){
+    np.title = null;
+    np.author = null;
+    np.img = null;
+    np.desc = null;
+    np.addPost = false;
+  }
+
+  var clearComment = function(post){
+    np.commentAuthor = null;
+    np.commentText = null;
+    post.addComment = false;
+  }
+
+  np.newPost = function(){
+    console.log('adding new post');
+    np.posts.push({
+      title: np.title,
+      author: np.author,
+      img: np.img,
+      desc: np.desc,
+      score: 0,
+      comments: []
+    })
+    console.log(np.posts);
+    clearForm();
+  }
+
+  np.newComment = function(post){
+    post.comments.push({
+      author: np.commentAuthor,
+      comment: np.commentText
+    })
+    console.log('new comment:',post.comments)
+    clearComment(post);
+  }
 }
